5a37cfd87aff75189618dce6d6726940b290d207
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-07 03:46:49 -0300

    Created class with functor for deleter

--- PunterosInteligentes/custom_deleter.cpp
+++ PunterosInteligentes/custom_deleter.cpp
@@ -1,26 +1,28 @@
 //  custom_deleter.cpp
 
-#include <cstdio>
 #include <memory>
 #include "strc.h"
 
+class D {
+public:
+	void operator()(const strc * o) {
+		printf("deleter: ");
+		if (o) printf("%s\n", o->value());
+		else printf("[null]\n");
+		fflush(stdout);
+		delete o;
+	}
+};
+
 // display object value from shared_ptr (with reference count)
 void disp(const std::shared_ptr<strc> & o);
-
-void deleter(const strc * o) {
-	printf("deleter: ");
-	if (o) printf("%s\n", o->value());
-	else printf("[null]\n");
-	fflush(stdout);
-	delete o;
-}
 
 void deleter_actions() {
 	puts("CUSTOM DELETER");
 	puts("--------------");
 
 	message("create shared_ptr");
-	std::shared_ptr<strc> a(new strc("thing"), &deleter);
+	std::shared_ptr<strc> a(new strc("thing"), D());
 	disp(a);
 
 	a.reset();
